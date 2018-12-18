74e2f7c96ea884806087c3664607360bd509573a
littlefxc <2575974990@qq.com>
2018-12-18 20:34:39 +0800

    feat(input_and_output.py):learn input and output on pythyon

---
+++ input_and_output.py
@@ -0,0 +1,18 @@
+# 输出
+
+# 用print()在括号中加上字符串，就可以向屏幕上输出指定的文字。
+print('hello, world')
+
+# print()函数也可以接受多个字符串，用逗号“,”隔开，就可以连成一串输出：
+# print()会依次打印每个字符串，遇到逗号“,”会输出一个空格
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+# print()也可以打印整数，或者计算结果:
+print(300)
+print('100 + 200 =', 100 + 200)
+
+# 输入
+
+# Python提供了一个input()，可以让用户输入字符串，并存放到一个变量里。比如输入用户的名字：
+name = input()
+print('name =', name)
