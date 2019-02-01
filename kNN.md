7b8baa9f1bee624242d428a7bb6b9f45a84c09d9
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-02-01 10:11:09 +0800

    kNN:分析&文档

--- kNN.md
+++ kNN.md
@@ -47,7 +47,7 @@ def classif0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    # 排序
+    # 降序排序
     sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 ```
--- kNN.py
+++ kNN.py
@@ -1,6 +1,7 @@
 from numpy import *
 import operator
 
+
 def createDataSet():
     # 数据集
     group = array([[1.0, 1.1],
@@ -16,19 +17,20 @@ def classif0(inX, dataSet, labels, k):
     # 计算距离
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat**2
+    sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
+    distances = sqDistances ** 0.5
     sortedDistIndicies = distances.argsort()
     classCount = {}
     # 选择距离最小的k个点
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    # 排序
-    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse=True)
+    # 降序排序
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+
 group, labels = createDataSet()
-result = classif0([0,0], group, labels, 3)
-print(result)
+result = classif0([0, 0], group, labels, 3)
+print(result) # B
