9316b0d32f8442a73d8ea6a720c78efd655f96fb
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-02-02 08:59:24 +0800

    kNN:添加文件处理函数

--- kNN.py
+++ kNN.py
@@ -31,6 +31,21 @@ def classif0(inX, dataSet, labels, k):
     return sortedClassCount[0][0]
 
 
-group, labels = createDataSet()
-result = classif0([0, 0], group, labels, 3)
-print(result) # B
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+# group, labels = createDataSet()
+# result = classif0([0, 0], group, labels, 3)
+# print(result) # B
