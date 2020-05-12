d6bb3fec34af29a82ae2c344f8459899a8325429
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-05-12 22:07:47 +0800

    feat(HelloWorld.py):在 learn 分支上学习 python 语言

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,18 +5,18 @@
 Version: 0.1
 Author: 冯雪超
 """
+import sys
+import this
+import turtle
+
+print(sys.version_info)
+print(sys.version)
 
 print('hello, world!')
-# print("你好,世界！")
-print('你好', '世界')
-# sep: 分隔符; end: 结尾字符串
-print('hello', 'world', sep=', ', end='!')
-print('goodbye, world', end='!\n')
-
-import turtle
 
 turtle.pensize(4)
 turtle.pencolor('red')
+
 turtle.forward(100)
 turtle.right(90)
 turtle.forward(100)
@@ -24,4 +24,5 @@ turtle.right(90)
 turtle.forward(100)
 turtle.right(90)
 turtle.forward(100)
+
 turtle.mainloop()
